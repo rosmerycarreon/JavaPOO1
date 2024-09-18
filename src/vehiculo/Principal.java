c4030012c966a12780d35203f81e776665d90cbd
rosmerycarreon <1392113@senati.pe>
2024-09-18 11:36:24 -0500

    agregar autor

--- src/vehiculo/Principal.java
+++ src/vehiculo/Principal.java
@@ -1,9 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package vehiculo;
 
+package vehiculo;
+//Autor: Rosmery Carreon Humpiri
 public class Principal {
     public static void main (String[] args){
         
